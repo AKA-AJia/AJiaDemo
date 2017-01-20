b295946efe8898b7c72724ffb61f763a5e7e9c5d
jiaming wu <aka_jia@163.com>
2017-01-20 20:59:51 +0800

    完成页面间的跳转

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,26 +1,16 @@
-//index.js
-//获取应用实例
-// var app = getApp()
-// Page({
-//   data: {
-//     // motto: '开启小程序',
-//     userInfo: {}
-//   },
-//   //事件处理函数
-//   bindViewTap: function() {
-//     wx.navigateTo({
-//       url: '../logs/logs'
-//     })
-//   },
-//   onLoad: function () {
-//     console.log('onLoad')
-//     var that = this
-//     //调用应用实例的方法获取全局数据
-//     app.getUserInfo(function(userInfo){
-//       //更新数据
-//       that.setData({
-//         userInfo:userInfo
-//       })
-//     })
-//   }
-// })
+Page({
+    onTap:function(){
+        wx.redirectTo({
+          url: '../posts/post',
+          success: function(res){
+            // success
+          },
+          fail: function() {
+            // fail
+          },
+          complete: function() {
+            // complete
+          }
+        })
+    }
+})
